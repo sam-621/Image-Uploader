15066ac885592719f6791577548a332128ccef27
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-09 17:44:41 -0600

    [ADD] Loader styles.

---
+++ src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import '../styles/components/loader.css';
+
+const Loader = () => {
+  return (
+    <div id="loader-container" className="Loader-div">
+      <div className="Loader-container">
+        <h1>Uploading</h1>
+        <div id="Loader-progress">
+          <div id="Loader-bar"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/container/Upload.jsx
+++ src/container/Upload.jsx
@@ -3,8 +3,27 @@ import '../styles/containers/upload.css';
 
 import Title from '../components/Title';
 import image from '../img/image.svg';
+import Loader from '../components/Loader';
 
 const Upload = () => {
+  var i = 0;
+  function move() {
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById('Loader-bar');
+      var width = 1;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elem.style.width = width + '%';
+        }
+      }
+    }
+  }
   const [fileName, setFileName] = useState();
 
   function OnChangeFileHandle(e) {
@@ -18,36 +37,43 @@ const Upload = () => {
     setTimeout(() => {
       container.classList.add('Hide');
     }, 1000);
+    setTimeout(() => {
+      document.getElementById('loader-container').classList.add('Show');
+      move();
+    }, 1000);
   }
 
   return (
-    <main id="Upload-main" className="Upload-main">
-      <div className="Upload-container">
-        <div className="Upload-title">
-          <Title Content="Upload your image" />
-          <p>File should be Jepg, Png...</p>
-        </div>
-        <form method="POST">
-          <div className="Upload-input-container">
-            <div className="Upload-img">
-              <img src={image} alt="Image drag and drop" />
-              <p>{fileName ? fileName : 'Drag & Drop your image here'}</p>
-            </div>
-            <input
-              id="file"
-              className="Upload-inputFile"
-              type="file"
-              onClick={() => {
-                setFileName(undefined);
-              }}
-              accept="image/*"
-              onChange={OnChangeFileHandle}
-            />
+    <>
+      <Loader />
+
+      <main id="Upload-main" className="Upload-main">
+        <div className="Upload-container">
+          <div className="Upload-title">
+            <Title Content="Upload your image" />
+            <p>File should be Jepg, Png...</p>
           </div>
-          <p className="Upload-Or">Or</p>
-          <div className="Upload-submit-container">
-            <label htmlFor="file">Chose a file</label>
-            {/* <input
+          <form method="POST">
+            <div className="Upload-input-container">
+              <div className="Upload-img">
+                <img src={image} alt="Image drag and drop" />
+                <p>{fileName ? fileName : 'Drag & Drop your image here'}</p>
+              </div>
+              <input
+                id="file"
+                className="Upload-inputFile"
+                type="file"
+                onClick={() => {
+                  setFileName(undefined);
+                }}
+                accept="image/*"
+                onChange={OnChangeFileHandle}
+              />
+            </div>
+            <p className="Upload-Or">Or</p>
+            <div className="Upload-submit-container">
+              <label htmlFor="file">Chose a file</label>
+              {/* <input
               id="file2"
               type="file"
               accept="image/*"
@@ -59,10 +85,11 @@ const Upload = () => {
                 setFileName(document.getElementById('file2').files[0].name);
               }}
             /> */}
-          </div>
-        </form>
-      </div>
-    </main>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
   );
 };
 
