0d4995c904d50c306291cde38d253a1576e3a5b8
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-10 09:36:19 -0600

    [ADD] copy text function.

--- src/container/Download.jsx
+++ src/container/Download.jsx
@@ -5,6 +5,19 @@ import Success from '../img/success.svg';
 import imageTest from '../img/SocialMedia-API.png';
 
 const Download = ({ URL }) => {
+  function CopyText() {
+    const textToCopy = document.getElementById('Text-to-copy');
+
+    const range = document.createRange();
+    range.selectNode(textToCopy);
+    window.getSelection().addRange(range);
+
+    const resultado = document.execCommand('copy');
+    if (!resultado) {
+      console.log('ERROR al intentar copiar el email');
+    }
+  }
+
   return (
     <main id="Download-main" className="Download-main ">
       <div className="Download-container">
@@ -16,8 +29,10 @@ const Download = ({ URL }) => {
           <img src={URL} width="338px" height="224.4px" alt="" />
         </div>
         <div className="Download-actions">
-          <p>{URL}</p>
-          <button>Copy Link</button>
+          <p id="Text-to-copy">{URL}</p>
+          <button id="btn-copy" onClick={CopyText}>
+            Copy Link
+          </button>
         </div>
       </div>
     </main>
