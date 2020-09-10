c0e5c35dfceca76ba3731cf0137894b51ba26f06
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-10 09:57:36 -0600

    [ADD] Config for production.

---
+++ webpack.config.prod.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'img',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      //   favicon: './src/assets/img/Logo.ico',
+    }),
+    new MiniCssExtractPlugin(),
+    new Dotenv(),
+  ],
+};
